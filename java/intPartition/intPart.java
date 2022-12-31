e026e886c11af8428658af3d3431fd4262cfe7a8
syxddd <syxddd@kalimain.tail.c>
2022-12-30 19:27:48 -0500

    added new program

---
+++ java/intPartition/intPart.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class Main
+{
+	public static void main(String [] args)
+	{
+		
+	}
+
+	public static int [] createArray(int size)
+	{
+		int [] array = new int[size];
+		for (int i = 0; i<size; i++)
+		{
+			array[i] = (int)(Math.Random*100);
+		}
+		return array;
+	}
+
+	public static int [] part(int [] array, int [] nArray)
+	{	
+		// []
+		// [8,6,4,3,5,1]
+		int i;
+		//start here
+		
+	}	
+
+}
