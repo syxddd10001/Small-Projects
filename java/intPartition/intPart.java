18a083a5e0121b438a74eb80985cc02649eb4a9a
monad <monad@kali-sub.monad.c>
2022-12-31 16:34:28 -0500

    finished intPartition

--- java/intPartition/intPart.java
+++ java/intPartition/intPart.java
@@ -1,29 +1,77 @@
 import java.util.Scanner;
 import java.lang.Math;
+import java.util.ArrayList;
 
 class Main
 {
 	public static void main(String [] args)
 	{
-		
+		mainmenu(); //calling mainmenu method
+	}
+	
+	public static void mainmenu()
+	{
+		ArrayList <Integer> array = createArray(10); //creating a random ArrayList of size 10
+		for (int i = 0; i<array.size(); i++) // displaying the ArrayList we just created to the user
+		{
+			System.out.print(array.get(i) + " ");
+		}
+		ArrayList <Integer> empty = new ArrayList<Integer>(); 
+
+		ArrayList <Integer> parted = part(array,empty);
+		System.out.println(" ");	
+		for (int i = 0; i<parted.size(); i++)
+		{
+			System.out.print(parted.get(i) + " ");
+		}
 	}
 
-	public static int [] createArray(int size)
+	public static ArrayList <Integer> createArray(int s)
 	{
-		int [] array = new int[size];
-		for (int i = 0; i<size; i++)
+		ArrayList <Integer> array = new ArrayList<Integer>();
+		for (int i = 0; i<s; i++)
 		{
-			array[i] = (int)(Math.Random*100);
+			 array.add((int)(Math.random()*100));
 		}
 		return array;
 	}
 
-	public static int [] part(int [] array, int [] nArray)
+	public static ArrayList <Integer> part(ArrayList<Integer> array, ArrayList<Integer> nArray)
 	{	
 		// []
 		// [8,6,4,3,5,1]
-		int i;
-		//start here
+		
+		if (array.size() == 0)
+		{
+			return nArray;
+		}
+		else
+		{
+			for (int i = 0; i<array.size();i++)
+			{
+				if ((array.get(i)%2)==0)
+				{
+					nArray.add(array.get(i));
+					array.remove(i);
+					return part(array,nArray);
+				}
+
+			}
+
+			for (int i = 0; i<array.size();i++)
+			{
+				if ((array.get(i)%2)==1)
+				{
+					nArray.add(array.get(i));
+					array.remove(i);
+					return part(array,nArray);
+				}
+			
+			}
+		
+		
+		}
+		return nArray;
 		
 	}	
 
